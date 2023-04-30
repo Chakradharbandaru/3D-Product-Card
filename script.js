04c70ebce073a10fbbcc2d0b609e199e194e9af2
Chakradhar <110602847+Chakradharbandaru@users.noreply.github.com>
2023-04-30 21:49:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
+
 // Items
 const title = document.querySelector(".title");
 const sneaker = document.querySelector(".sneaker img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
+
 
 // Moving animation event
 container.addEventListener("mousemove", (e) => {
